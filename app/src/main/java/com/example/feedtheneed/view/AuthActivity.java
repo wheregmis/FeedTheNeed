c96b8474e6fa5ad9ed57bff599ee0dcaf399c2ea
Sabin Regmi <xawbeenregmi@gmail.com>
2022-07-05 18:15:58 -0400

    Adding Auth Activity, layout and firebase dependencies

---
+++ app/src/main/java/com/example/feedtheneed/view/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.example.feedtheneed.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.feedtheneed.R;
+
+public class AuthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
