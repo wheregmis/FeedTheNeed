d92d2436592d5f9093198ba4fd81247ba444a163
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-07-20 18:55:16 -0400

    added additional information activity

---
+++ app/src/main/java/com/example/feedtheneed/presentation/user/AdditionalInformationActivity.java
@@ -0,0 +1,16 @@
+package com.example.feedtheneed.presentation.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.feedtheneed.R;
+
+public class AdditionalInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additional_information);
+    }
+}
