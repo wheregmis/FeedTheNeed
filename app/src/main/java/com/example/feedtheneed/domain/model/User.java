f5e10900c644c8bfb012bc473e8f8956b3ca695f
Sabin Regmi <xawbeenregmi@gmail.com>
2022-07-21 00:51:40 -0400

    Updated User Model
    -- added user location in model
    -- added if user is restaurant or not in model

--- app/src/main/java/com/example/feedtheneed/domain/model/User.java
+++ app/src/main/java/com/example/feedtheneed/domain/model/User.java
@@ -1,9 +1,30 @@
 package com.example.feedtheneed.domain.model;
 
+
+import com.google.android.gms.maps.model.LatLng;
+
 public class User {
     private String userId;
     private String userFullName;
     private String userEmail;
+    private boolean isRestaurant;
+    private LatLng userLatLng;
+
+    public LatLng getUserLatLng() {
+        return userLatLng;
+    }
+
+    public void setUserLatLng(LatLng userLatLng) {
+        this.userLatLng = userLatLng;
+    }
+
+    public boolean isRestaurant() {
+        return isRestaurant;
+    }
+
+    public void setRestaurant(boolean restaurant) {
+        isRestaurant = restaurant;
+    }
 
     public String getUserId() {
         return userId;
@@ -32,5 +53,20 @@ public class User {
     public User(String userFullName, String userEmail) {
         this.userFullName = userFullName;
         this.userEmail = userEmail;
+    }
+
+    public User(String userId, String userFullName, String userEmail, boolean isRestaurant) {
+        this.userId = userId;
+        this.userFullName = userFullName;
+        this.userEmail = userEmail;
+        this.isRestaurant = isRestaurant;
+    }
+
+    public User(String userId, String userFullName, String userEmail, boolean isRestaurant, LatLng userLatLng) {
+        this.userId = userId;
+        this.userFullName = userFullName;
+        this.userEmail = userEmail;
+        this.isRestaurant = isRestaurant;
+        this.userLatLng = userLatLng;
     }
 }
