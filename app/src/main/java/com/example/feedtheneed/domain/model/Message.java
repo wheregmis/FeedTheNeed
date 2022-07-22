4477e0751249f9a4ef11aa7e5deb5e675f4442f2
Namrata Barot <namrata.barot3@gmail.com>
2022-07-22 18:55:13 -0400

    design changes

---
+++ app/src/main/java/com/example/feedtheneed/domain/model/Message.java
@@ -0,0 +1,31 @@
+package com.example.feedtheneed.domain.model;
+
+
+public class Message {
+
+    private String content, username;
+    public Message(){
+
+    }
+
+    public Message(String content, String username) {
+        this.content = content;
+        this.username = username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
