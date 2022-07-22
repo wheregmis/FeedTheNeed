250f8f1508dd6541b07566eb67b3940630fed1be
Ishara Abeykoon <isharaux@gmail.com>
2022-07-22 19:04:59 -0400

    adding menu resource

--- app/src/main/java/com/example/feedtheneed/HomeActivity.java
+++ app/src/main/java/com/example/feedtheneed/HomeActivity.java
@@ -1,20 +1,17 @@
 package com.example.feedtheneed;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.feedtheneed.databinding.ActivityHomeBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
