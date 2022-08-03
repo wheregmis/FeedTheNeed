570318643aab2c993ba96d5e201ed58cde707858
Namrata Barot <namrata.barot3@gmail.com>
2022-08-02 20:18:33 -0400

    check auth condition and redirecting to activity accordingly

--- app/src/main/java/com/example/feedtheneed/SplashActivity.java
+++ app/src/main/java/com/example/feedtheneed/SplashActivity.java
@@ -6,10 +6,15 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.feedtheneed.presentation.authentication.AuthActivity;
 import com.google.android.gms.location.LocationServices;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +23,28 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
-                //i.putExtra("Lat",currentLocation.getLatitude());
-                startActivity(i);
-                finish();
+                FirebaseApp.initializeApp(SplashActivity.this);
+
+                // Initialize firebase auth
+                firebaseAuth= FirebaseAuth.getInstance();
+                // Initialize firebase user
+                FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+                // Check condition
+                if(firebaseUser!=null)
+                {
+                    // When user already sign in
+                    // redirect to profile activity
+                    startActivity(new Intent(SplashActivity.this, HomeActivity.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)); finish();
+                }
+                else
+                {
+                    Intent i = new Intent(SplashActivity.this, AuthActivity.class);
+                    //i.putExtra("Lat",currentLocation.getLatitude());
+                    startActivity(i);
+                    finish();
+                }
+
             }
         }, SPLASH_TIME_OUT);
     }
