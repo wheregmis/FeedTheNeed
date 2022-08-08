0792926c9331c229c55c68f9f55c35014b619c65
Sabin Regmi <xawbeenregmi@gmail.com>
2022-08-07 21:57:59 -0400

    Food Rating View Added

---
+++ app/src/main/java/com/example/feedtheneed/data/repository/FoodRatingRepoImplementation.java
@@ -0,0 +1,42 @@
+package com.example.feedtheneed.data.repository;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.feedtheneed.domain.model.Event;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.security.Key;
+import java.util.HashMap;
+
+public class FoodRatingRepoImplementation {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public Task<DocumentReference> insertFoodRatingValue(String restaurantId, String userId, String comment) {
+        HashMap<String, String> foodRating = new HashMap<String, String>();
+
+        foodRating.put("restaurantId", restaurantId);
+        foodRating.put("userId", userId);
+        foodRating.put("comment", comment);
+
+        return db.collection("ratings")
+                .add(foodRating)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Food Rating", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Food Rating", "Error adding document", e);
+                    }
+                });
+    }
+}
